1060ef4405c373deff325e8626357a5b5f850312
dangzan <dangozan@gmail.com>
2023-08-11 22:47:50 -0400

    chore: change file name to complete previous commit

--- src/TimeSlotGroup.js
+++ src/TimeSlotGroup.js
@@ -1,38 +1,36 @@
 import clsx from 'clsx'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 
 import BackgroundWrapper from './BackgroundWrapper'
 
-export default class TimeSlotGroup extends Component {
-  render() {
-    const {
-      renderSlot,
-      resource,
-      group,
-      getters,
-      components: { timeSlotWrapper: Wrapper = BackgroundWrapper } = {},
-    } = this.props
+export default function TimeSlotGroup(props) {
+  const {
+    renderSlot,
+    resource,
+    group,
+    getters,
+    components: { timeSlotWrapper: Wrapper = BackgroundWrapper } = {},
+  } = props
 
-    const groupProps = getters ? getters.slotGroupProp(group) : {}
-    return (
-      <div className="rbc-timeslot-group" {...groupProps}>
-        {group.map((value, idx) => {
-          const slotProps = getters ? getters.slotProp(value, resource) : {}
-          return (
-            <Wrapper key={idx} value={value} resource={resource}>
-              <div
-                {...slotProps}
-                className={clsx('rbc-time-slot', slotProps.className)}
-              >
-                {renderSlot && renderSlot(value, idx)}
-              </div>
-            </Wrapper>
-          )
-        })}
-      </div>
-    )
-  }
+  const groupProps = getters ? getters.slotGroupProp(group) : {}
+  return (
+    <div className="rbc-timeslot-group" {...groupProps}>
+      {group.map((value, idx) => {
+        const slotProps = getters ? getters.slotProp(value, resource) : {}
+        return (
+          <Wrapper key={idx} value={value} resource={resource}>
+            <div
+              {...slotProps}
+              className={clsx('rbc-time-slot', slotProps.className)}
+            >
+              {renderSlot && renderSlot(value, idx)}
+            </div>
+          </Wrapper>
+        )
+      })}
+    </div>
+  )
 }
 
 TimeSlotGroup.propTypes = {
