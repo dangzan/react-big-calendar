1d8b4dfa1bb32140f0587d5b0afeb6862bd5d59c
dangzan <dangozan@gmail.com>
2023-08-11 21:51:05 -0400

    refactor: component TimeSlotGroup from class to function

---
+++ src/TimeSlotGroup2.js
@@ -0,0 +1,42 @@
+import clsx from 'clsx'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import BackgroundWrapper from './BackgroundWrapper'
+
+export default function TimeSlotGroup(props) {
+  const {
+    renderSlot,
+    resource,
+    group,
+    getters,
+    components: { timeSlotWrapper: Wrapper = BackgroundWrapper } = {},
+  } = props
+
+  const groupProps = getters ? getters.slotGroupProp(group) : {}
+  return (
+    <div className="rbc-timeslot-group" {...groupProps}>
+      {group.map((value, idx) => {
+        const slotProps = getters ? getters.slotProp(value, resource) : {}
+        return (
+          <Wrapper key={idx} value={value} resource={resource}>
+            <div
+              {...slotProps}
+              className={clsx('rbc-time-slot', slotProps.className)}
+            >
+              {renderSlot && renderSlot(value, idx)}
+            </div>
+          </Wrapper>
+        )
+      })}
+    </div>
+  )
+}
+
+TimeSlotGroup.propTypes = {
+  renderSlot: PropTypes.func,
+  group: PropTypes.array.isRequired,
+  resource: PropTypes.any,
+  components: PropTypes.object,
+  getters: PropTypes.object,
+}
